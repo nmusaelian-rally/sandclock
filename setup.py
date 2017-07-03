06da98777838a0cec4b7f716305d4388de351231
nmusaelian-rally <nmusaelian@rallydev.com>
2017-07-03 10:00:51 -0600

    set version to 0.7.1 in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name = 'sandclock',
     packages = ['sandclock'],
-    version = '0.7.0',  # Ideally should be same as your GitHub release tag varsion
+    version = '0.7.1',  # Ideally should be same as your GitHub release tag varsion
     description = 'self documenting methods for time manipulation',
     author = 'NM',
     author_email = 'nickrally2009@gmail.com',
