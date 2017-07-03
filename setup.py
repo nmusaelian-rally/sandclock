7629f0c843ca8f356a998cace96b82a8af13dbfa
nmusaelian-rally <nmusaelian@rallydev.com>
2017-07-03 09:57:17 -0600

    changes to setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'sandclock',
+    packages = ['sandclock'],
+    version = '0.7.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'self documenting methods for time manipulation',
+    author = 'NM',
+    author_email = 'nickrally2009@gmail.com',
+    url = 'https://github.com/nmusaelian-rally/sandclock',
+    download_url = 'https://github.com/nmusaelian-rally/sandclock/archive/v0.7.1.tar.gz',
+    keywords = ['convert', 'time'],
+    classifiers = [],
+)
