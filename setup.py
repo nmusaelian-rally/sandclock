3b20ed705dce500120d5ee7f3d5e23ebab1b990a
nmusaelian-rally <nmusaelian@rallydev.com>
2017-07-03 10:46:04 -0600

    changed version to 0.7.2 in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name = 'sandclock',
     packages = ['sandclock'],
-    version = '0.7.1',  # Ideally should be same as your GitHub release tag varsion
+    version = '0.7.2',  # Ideally should be same as your GitHub release tag varsion
     description = 'self documenting methods for time manipulation',
     author = 'NM',
     author_email = 'nickrally2009@gmail.com',
     url = 'https://github.com/nmusaelian-rally/sandclock',
-    download_url = 'https://github.com/nmusaelian-rally/sandclock/archive/v0.7.1.tar.gz',
+    download_url = 'https://github.com/nmusaelian-rally/sandclock/archive/v0.7.2.tar.gz',
     keywords = ['convert', 'time'],
     classifiers = [],
 )
